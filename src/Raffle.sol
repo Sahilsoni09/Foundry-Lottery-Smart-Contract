72521754946dd83229f6778f0123719a3919eed4
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-01-23 13:34:19 +0000

    Chainlink VRF integration

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -25,47 +25,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {VRFConsumerBaseV2Plus} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 /**
-* @title A sample raffle contract
-* @author Sahil Soni
-* @notice This contract is for creating a smaple raffle
-* @dev It implement Chainlink VRF V2.5 & Chainlink Automation  
+ * @title A sample raffle contract
+ * @author Sahil Soni
+ * @notice This contract is for creating a smaple raffle
+ * @dev It implement Chainlink VRF V2.5 & Chainlink Automation
  */
 
-contract Raffle {
-
+contract Raffle is VRFConsumerBaseV2Plus {
     /*Errors */
-    error Raffle__SendMoreToEnterRaffle(); // add prefix to know from which contract this error is coming 
+    error Raffle__SendMoreToEnterRaffle(); // add prefix to know from which contract this error is coming
 
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
     //@dev the duration of the lottery in seconds
-    uint256 private immutable i_interval; 
-    address payable[] private s_players; // to keep track of players who enter raffle 
-    uint256 private s_lastTimeStamp; 
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players; // to keep track of players who enter raffle
+    uint256 private s_lastTimeStamp;
 
     /**Events */
-    event RaffleEntered(address indexed player); 
+    event RaffleEntered(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32  callbackGasLimit
+
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     // external function are more gas efficient than public functions
-    function enterRaffle() external payable{
+    function enterRaffle() external payable {
         // require( msg.value >= i_entranceFee, "Not enough ETH Sent!");
 
         // in solidity version 0.8.4, Custom errors are more gas efficient inspite of storing strings
-        if(msg.value < i_entranceFee){
+        if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
 
-        // in solidity version 0.8.26, custom error in require statement, but above way is more gas efficient 
-        // require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle()); 
-        
-        s_players.push(payable(msg.sender)); // Rule of thumb whenever you update storage variable emit events
+        // in solidity version 0.8.26, custom error in require statement, but above way is more gas efficient
+        // require(msg.value >= i_entranceFee, Raffle__SendMoreToEnterRaffle());
 
+        s_players.push(payable(msg.sender)); // Rule of thumb whenever you update storage variable emit events
 
         // Reason for using events:
         //1.  Makes migration easier
@@ -78,13 +94,33 @@ contract Raffle {
     // automatically call pickWinner function
     function pickWinner() external {
         // check to see if enough time has passed
-        if((block.timestamp - s_lastTimeStamp) < i_interval){
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+
+        // Getting a Random number is a two transaction process
+        // 1. Request a random number
+        // 2. get random number using fulfil function
+
+        VRFV2PlusClient.RandomWordsRequest memory request =  VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATION,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+        
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override{}
+
     /**Getter function */
-    function getEntranceFee() public view returns (uint256){
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 }
