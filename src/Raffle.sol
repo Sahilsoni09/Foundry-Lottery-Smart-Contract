33acdf60332640e18400b439cb3cbd63c350976f
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-01-23 02:28:37 +0000

    SendMoreToEnterRaffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,27 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
@@ -9,7 +33,33 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
-    function enterRaffle() public {}
+
+    /*Errors */
+    error SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable{
+        // require( msg.value >= i_entranceFee, "Not enough ETH Sent!");
+
+        // in solidity version 0.8.4, Custom errors are more gas efficient inspite of storing strings
+        if(msg.value < i_entranceFee){
+            revert SendMoreToEnterRaffle();
+        }
+
+        // in solidity version 0.8.26, custom error in require statement, but above way is more gas efficient 
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle()); 
+        
+    }
 
     function pickWinner() public {}
+
+    /**Getter function */
+    function getEntranceFee() public view returns (uint256){
+        return i_entranceFee;
+    }
 }
